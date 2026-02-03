d5c86b204596cfe6ad4163bc655cf2ef3c612545
PAblo Fernandez Cabezas <pfc28@alu.ua.es>
2026-02-03 14:19:22 +0100

    Implementada interacción con el usuario en Program.cs

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,26 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string respuesta;
+            do
+            {
+                Console.WriteLine("¿Qué conversión desea realizar? (min/seg) o 'salir':");
+                respuesta = Console.ReadLine();
+
+                if (respuesta == "min")
+                {
+                    Console.WriteLine("Introduce la cantidad de minutos:");
+                    double m = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0} minutos son {1} segundos.", m, HadaP1.Minutes2Seconds(m));
+                }
+                else if (respuesta == "seg")
+                {
+                    Console.WriteLine("Introduce la cantidad de segundos:");
+                    double s = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0} segundos son {1} minutos.", s, HadaP1.Seconds2Minutes(s));
+                }
+
+                [cite_start]} while (respuesta != "salir"); [cite: 1193]
         }
     }
 }
